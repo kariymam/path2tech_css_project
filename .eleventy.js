cfd733e95d926601515efcfdb00f945ed8d5e580
kariymam <59487728+kariymam@users.noreply.github.com>
2024-11-28 12:54:52 -0500

    Got scss working as a template with addExtension

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,36 @@
+const sass = require("sass");
+const path = require("path");
+
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('src/assets').addPassthroughCopy('src/css/*.css');
-    eleventyConfig.addWatchTarget("src/css/*.scss");
+    eleventyConfig.addPassthroughCopy('./src/assets');
+    // eleventyConfig.addWatchTarget("./src/sass");
+
+    // Add SASS support
+    eleventyConfig.addTemplateFormats("scss");
+
+    eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css",
+
+		compile: function (inputContent, inputPath) {
+			let parsed = path.parse(inputPath);
+			let result = sass.compileString(inputContent, {
+				loadPaths: [
+					parsed.dir || ".",
+					this.config.dir.includes
+				]
+			});
+
+			let dependencies = result.loadedUrls.filter(dep => dep.protocol === "file:").map(entry => {
+				return path.relative(".", entry.pathname);
+			});
+			this.addDependencies(inputPath, dependencies);
+
+			return (data) => {
+				return result.css
+			};
+		}
+	});
+
 
     return {
         dir: {
